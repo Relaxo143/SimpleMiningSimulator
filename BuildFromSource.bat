9a85670d3cb10e4ce68b703a5b64bac2254c6215
Relaxo143 <Kalin.Lalov@abv.bg>
2020-01-31 18:06:31 +0200

    Add file for easy building

---
+++ BuildFromSource.bat
@@ -0,0 +1,4 @@
+set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework64\v4.0.30319
+set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319
+msbuild /property:Configuration=Release SimpleMinerSim.csproj
+PAUSE
